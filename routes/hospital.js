8422aac358148307cc4ad6ced92b3d3dcf12fee2
EngelberAmaya <engelberamaya@gmail.com>
2020-08-19 23:37:13 -0430

    the doctor's get was added by id

--- routes/hospital.js
+++ routes/hospital.js
@@ -46,6 +46,69 @@ app.get('/', (req, res, next) => {
 });
 
 
+// ============================================
+// Obtener todos los hospitales- Sin paginación
+// ============================================
+app.get('/hosp', (req, res, next) => {
+
+	
+	Hospital.find({})			
+			.populate('usuario', 'nombre email')
+		    .exec(
+		   		(err, hospitales) =>{
+					if (err) {
+						return res.status(500).json({
+							ok:false,
+							mensaje: 'Error cargando hospital',
+							errors: err
+						});
+					}
+
+					Hospital.count({}, (err, conteo) => {
+						res.status(200).json({
+							ok:true,
+							hospitales: hospitales,
+							total: conteo
+						});
+					});
+
+					
+				});
+
+});
+
+
+// ==========================================
+//  Obtener Hospital por ID
+// ==========================================
+app.get('/:id', (req, res) => {
+
+    var id = req.params.id;
+
+    Hospital.findById(id)
+        .populate('usuario', 'nombre img email')
+        .exec((err, hospital) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: false,
+                    mensaje: 'Error al buscar hospital',
+                    errors: err
+                });
+            }
+
+            if (!hospital) {
+                return res.status(400).json({
+                    ok: false,
+                    mensaje: 'El hospital con el id ' + id + 'no existe',
+                    errors: { message: 'No existe un hospital con ese ID' }
+                });
+            }
+            res.status(200).json({
+                ok: true,
+                hospital: hospital
+            });
+        })
+})
 
 
 
